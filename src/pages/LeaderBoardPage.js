c51b1610283eae063a9aeebb6c45dde98a0a13dd
ZIXIANG JI <jerryji040506@ucla.edu>
2023-09-30 14:29:31 -0700

    paging

--- src/pages/LeaderBoardPage.js
+++ src/pages/LeaderBoardPage.js
@@ -1,14 +1,28 @@
 import { useEffect, useState } from 'react';
 
-import { Table, Grid, Container, Stack, Typography, TableContainer } from '@mui/material';
+import { Grid, Container, Stack, Typography, TablePagination } from '@mui/material';
 import { Helmet } from 'react-helmet-async';
 import { AppNewsUpdate } from '../sections/@dashboard/app';
+import Scrollbar from '../components/scrollbar';
 
 export default function LeaderBoardPage() {
   const [leadingPosts, setLeadingPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [total, setTotal] = useState(0);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+    console.log(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setPage(0);
+    setRowsPerPage(parseInt(event.target.value, 10));
+  };
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/job/api/leaderboard')
+    fetch(`http://127.0.0.1:8000/job/api/leaderboard?page=${page}&per_page=${rowsPerPage}`)
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -17,12 +31,15 @@ export default function LeaderBoardPage() {
       })
       .then((data) => {
         if (data) {
+          setTotal(data.total_num);
           // Extract job post objects from the numbered keys
-          const jobPosts = Object.values(data);
-
+          // Extract job post objects from the numbered keys while excluding "total_num"
+          const jobPosts = Object.keys(data)
+            .filter((key) => key !== 'total_num')
+            .map((key) => data[key]);
           // Set the job posts in the state
           setLeadingPosts(jobPosts);
-          console.log(jobPosts);
+          // console.log(jobPosts);
         } else {
           throw new Error('Invalid data');
         }
@@ -30,7 +47,7 @@ export default function LeaderBoardPage() {
       .catch((error) => {
         throw new Error(`Error Fetching the leaderboard information: ${error.message}`);
       });
-  }, []);
+  }, [page, rowsPerPage]);
 
   return (
     <>
@@ -47,7 +64,18 @@ export default function LeaderBoardPage() {
 
         <Grid container spacing={3}>
           <Grid item xs={30} md={16} lg={16}>
-            <AppNewsUpdate list={leadingPosts} />
+            <Scrollbar>
+              <AppNewsUpdate list={leadingPosts} />
+            </Scrollbar>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 20]}
+              component="div"
+              count={total}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
           </Grid>
         </Grid>
       </Container>
